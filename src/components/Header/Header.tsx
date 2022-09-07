5c3976c313192a599b90ae2ba2a95ead8797d026
Arnel Jan Sarmiento <aesarmiento@up.edu.ph>
2022-09-07 13:47:46 +0800

    Initial overall layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Header() {
+  return (
+    <div className="App">
+        <h1>Image Classifications</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+export interface  indexProps {
+}
+
+export function index (props:  indexProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
