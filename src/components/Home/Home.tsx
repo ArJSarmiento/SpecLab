7f986fb7bcd9051ef0d8268f160be5f03bafb7af
Arnel Jan Sarmiento <aesarmiento@up.edu.ph>
2022-10-14 21:09:18 +0800

    Responsiveness Issue fixes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,5 @@
 import {
     BrowserRouter as Router,
-    Routes,
-    Route,
     Link
   } from 'react-router-dom';
 import './index.css';
@@ -10,28 +8,29 @@ function Home() {
     return ( 
         <div className="container">
             <main>
-                <div>
-                <section className="mainText">
-                    <article>
-                    <span className="whiteText">SpecLab</span> AI
-                    </article>
-                    <article>
-                    Predictions
-                    </article>
-                </section>
-                <section className="subText">
-                    <article>
-                    A Next Generation Image Recognition Platform with machine learning technology.
-                    </article>
-                </section>
+                <div className="content">
+                    <section className="mainText">
+                        <article>
+                        <span className="whiteText">SpecLab</span> AI
+                        </article>
+                        <article>
+                        Predictions
+                        </article>
+                    </section>
+                    <section className="subText">
+                        <article>
+                            A Next Generation Image Recognition Platform with machine learning technology.
+                        </article>
+                    </section>
 
-                <Link to="/predict" className="btn">
-                    <span>
-                        Predict Now
-                    </span>
-                    <img src="arrow-right.png" alt="arrow" />
-                </Link>
+                    <Link to="/predict" className="btn">
+                        <span>
+                            Predict Now
+                        </span>
+                        <img src="arrow-right.png" alt="arrow" />
+                    </Link>
                 </div>
+                <div className="blur"></div>
             </main> 
         </div>
     );
